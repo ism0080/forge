20ec41fa9859573feb6d5eeb566ad968278fd667
Isaac Mackle <isaac.mackle@gmail.com>
2026-06-14 17:10:55 +1200

    cors

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -2,14 +2,34 @@ import { createServer } from "node:http";
 import { NodeHttpServer, NodeRuntime } from "@effect/platform-node";
 import * as NodeServices from "@effect/platform-node/NodeServices";
 import { Effect, Layer } from "effect";
-import { HttpServer, HttpRouter } from "effect/unstable/http";
+import { HttpMiddleware, HttpServer, HttpRouter } from "effect/unstable/http";
 import { ServerConfigLayer, ServerConfigService } from "./config/server.js";
 import { LocalFileDatabaseLayer } from "./db/local-file.js";
 import { DbEventsConsoleTapLayer, DbEventsInMemoryLayer } from "./db/events.js";
 import { HttpRouterLayer, RoutesLayer } from "./routes/index.js";
 import { LocalFileStorageLayer } from "./storage/local-file.js";
 
+const CorsMiddleware = HttpRouter.middleware(
+  HttpMiddleware.cors({
+    allowedOrigins: [],
+    allowedMethods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE", "OPTIONS"],
+    allowedHeaders: [
+      "Content-Type",
+      "Authorization",
+      "X-Requested-With",
+      "X-Forge-User-Id",
+      "X-Forge-User-Email",
+      "X-Forge-User-Name",
+      "X-Forge-User-Team",
+    ],
+    credentials: true,
+    maxAge: 86400,
+  }),
+  { global: true },
+);
+
 const AppLayer = HttpRouter.serve(RoutesLayer).pipe(
+  Layer.provideMerge(CorsMiddleware),
   Layer.provide(HttpRouterLayer),
   Layer.provide(DbEventsConsoleTapLayer),
   Layer.provide(LocalFileDatabaseLayer),
